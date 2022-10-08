7e2fa70255961743b21b174ab0162cf6722aa652
Yashashwee <yashashwee99@gmail.com>
2022-10-09 00:18:44 +0530

    Added player exited logic

--- app.py
+++ app.py
@@ -56,5 +56,12 @@ def nextKey(data):
     sio.emit('begin', gdata)
 
 
+@sio.event
+def disconnect():
+    global players
+    sio.emit("error", "Other player diconneted")
+    players = []
+
+
 if __name__ == '__main__':
     sio.run(app, debug=True, host='0.0.0.0', port=5004)
