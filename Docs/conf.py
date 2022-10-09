0544b83bb149589a70370bb60aa718a6da2fb788
Warhead <avatsal38@gmail.com>
2022-10-09 16:04:44 +0530

    some changes

--- Docs/conf.py
+++ Docs/conf.py
@@ -32,7 +32,7 @@ extensions = ['sphinx.ext.autodoc'
 ,'sphinx.ext.githubpages'
 ,'sphinx.ext.napoleon']
 
-templates_path = ['_templates']
+#templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
@@ -41,4 +41,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
